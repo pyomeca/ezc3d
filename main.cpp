4136d5e86d2bddabd05f12df19a9fc4317b31d89
Pariterre <pariterre@hotmail.com>
2018-05-07 21:32:36 -0400

    changed name of 'file' to 'c3d' in example file

--- main.cpp
+++ main.cpp
@@ -4,22 +4,22 @@
 
 int main()
 {
-    ezC3D file("../markers_analogs.c3d");
+    ezC3D c3d("../markers_analogs.c3d");
 
 
     // Print the read C3D
-    file.header()->print();
-    file.parameters()->print();
-    file.data()->print();
+    c3d.header()->print();
+    c3d.parameters()->print();
+    c3d.data()->print();
 
     // Test for specific frame Point
-    std::string namePoint(file.data()->frame(10).points()->point(0).name());
-    ezC3D::Data::Points3d::Point p(file.data()->frame(10).points()->point(namePoint));
+    std::string namePoint(c3d.data()->frame(10).points()->point(0).name());
+    ezC3D::Data::Points3d::Point p(c3d.data()->frame(10).points()->point(namePoint));
     p.print();
 
     // Test for specific subframe Analog
-    std::string nameAnalog(file.data()->frame(10).analogs()->subframe(2).channel(2).name());
-    ezC3D::Data::Analogs::SubFrame::Channel c(file.data()->frame(10).analogs()->subframe(2).channel(nameAnalog));
+    std::string nameAnalog(c3d.data()->frame(10).analogs()->subframe(2).channel(2).name());
+    ezC3D::Data::Analogs::SubFrame::Channel c(c3d.data()->frame(10).analogs()->subframe(2).channel(nameAnalog));
     c.print();
 
     // Terminate
