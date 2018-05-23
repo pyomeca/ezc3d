2fa6bf0cc6937656c9f2249dc4e1f3e71c2bbe5a
Pariterre <pariterre@hotmail.com>
2018-05-23 17:46:32 -0400

    Optimized code for speed while reading a file

--- example/main.cpp
+++ example/main.cpp
@@ -1,32 +1,37 @@
 
 #include <vector>
 #include "ezc3d.h"
+#include <chrono>
 
 int main()
 {
-    ezc3d::c3d c3d("../markers_analogs.c3d");
+    auto start = std::chrono::high_resolution_clock::now();
+    for (int i = 0; i < 10; ++i)
+        ezc3d::c3d c3d("../markers_analogs.c3d");
+    auto stop = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
+
+    std::cout << double(duration.count()) /1000/1000 << std::endl;
+//    // Print the read c3d
+//    c3d.header().print();
+//    //c3d.parameters().print();
+//    //c3d.data().print();
 
 
-    // Print the read c3d
-    c3d.header().print();
-    //c3d.parameters().print();
-    //c3d.data().print();
+//    // Test for specific frame Point
+//    std::string namePoint(c3d.data().frame(10).points().point(0).name());
+//    ezc3d::DataNS::Points3dNS::Point p(c3d.data().frame(10).points().point(namePoint));
+//    p.print();
 
+//    // Test for specific subframe Analog
+//    std::string nameAnalog(c3d.data().frame(10).analogs().subframe(2).channel(2).name());
+//    ezc3d::DataNS::AnalogsNS::Channel c(c3d.data().frame(10).analogs().subframe(2).channel(nameAnalog));
+//    c.print();
+//    c = c3d.data().frame(10).analogs().subframe(2).channel(3);
+//    c.print();
 
-    // Test for specific frame Point
-    std::string namePoint(c3d.data().frame(10).points().point(0).name());
-    ezc3d::DataNS::Points3dNS::Point p(c3d.data().frame(10).points().point(namePoint));
-    p.print();
-
-    // Test for specific subframe Analog
-    std::string nameAnalog(c3d.data().frame(10).analogs().subframe(2).channel(2).name());
-    ezc3d::DataNS::AnalogsNS::Channel c(c3d.data().frame(10).analogs().subframe(2).channel(nameAnalog));
-    c.print();
-    c = c3d.data().frame(10).analogs().subframe(2).channel(3);
-    c.print();
-
-    // Terminate
-    std::cout << "Successfully read the c3d file" << std::endl;
+//    // Terminate
+//    std::cout << "Successfully read the c3d file" << std::endl;
 
     return 0;
 }
