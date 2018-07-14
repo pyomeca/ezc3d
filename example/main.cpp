8b9ebd8f5b3abd1981e753bb4fb3454a8954c1a1
Pariterre <pariterre@hotmail.com>
2018-07-14 12:38:37 -0400

    Proper example compilation

--- example/main.cpp
+++ example/main.cpp
@@ -6,34 +6,37 @@
 int main()
 {
     auto start = std::chrono::high_resolution_clock::now();
-    for (int i = 0; i < 10; ++i){
-        ezc3d::c3d c3d("../markers_analogs.c3d");
+    for (int i = 0; i < 3; ++i){
+        ezc3d::c3d c3d("markers_analogs.c3d");
         std::cout << "coucou" << std::endl;
     }
     auto stop = std::chrono::high_resolution_clock::now();
     auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
 
     std::cout << double(duration.count()) /1000/1000 << std::endl;
-//    // Print the read c3d
-//    c3d.header().print();
-//    //c3d.parameters().print();
-//    //c3d.data().print();
 
 
-//    // Test for specific frame Point
-//    std::string namePoint(c3d.data().frame(10).points().point(0).name());
-//    ezc3d::DataNS::Points3dNS::Point p(c3d.data().frame(10).points().point(namePoint));
-//    p.print();
+    // Print the read c3d
+    ezc3d::c3d c3d("markers_analogs.c3d");
+    c3d.header().print();
+    c3d.parameters().print();
+    //c3d.data().print(); // Commented for time issue (it is long to plot all the data)
 
-//    // Test for specific subframe Analog
-//    std::string nameAnalog(c3d.data().frame(10).analogs().subframe(2).channel(2).name());
-//    ezc3d::DataNS::AnalogsNS::Channel c(c3d.data().frame(10).analogs().subframe(2).channel(nameAnalog));
-//    c.print();
-//    c = c3d.data().frame(10).analogs().subframe(2).channel(3);
-//    c.print();
 
-//    // Terminate
-//    std::cout << "Successfully read the c3d file" << std::endl;
+    // Test for specific frame Point
+    std::string namePoint(c3d.data().frame(10).points().point(0).name());
+    ezc3d::DataNS::Points3dNS::Point p(c3d.data().frame(10).points().point(namePoint));
+    p.print();
+
+    // Test for specific subframe Analog
+    std::string nameAnalog(c3d.data().frame(10).analogs().subframe(2).channel(2).name());
+    ezc3d::DataNS::AnalogsNS::Channel c(c3d.data().frame(10).analogs().subframe(2).channel(nameAnalog));
+    c.print();
+    c = c3d.data().frame(10).analogs().subframe(2).channel(3);
+    c.print();
+
+    // Terminate
+    std::cout << "Successfully read and printed the c3d file" << std::endl;
 
     return 0;
 }
