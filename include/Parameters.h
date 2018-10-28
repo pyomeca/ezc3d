14066a5fb0ad89d9937ccc8d28d4720b0672d7c2
Pariterre <pariterre@hotmail.com>
2018-10-28 13:40:58 -0400

    Moved Group to a proper file and documented it

--- include/Parameters.h
+++ include/Parameters.h
@@ -1,7 +1,7 @@
 #ifndef PARAMETERS_H
 #define PARAMETERS_H
 
-#include <Parameter.h>
+#include <Group.h>
 
 class EZC3D_API ezc3d::ParametersNS::Parameters{
 public:
@@ -30,38 +30,6 @@ protected:
     int _checksum;         // Byte 2 ==> should be 80 if it is a c3d
     int _nbParamBlock;      // Byte 3 ==> Number of parameter blocks to follow
     int _processorType;     // Byte 4 ==> Processor type (83 + [1 Inter, 2 DEC, 3 MIPS])
-};
-
-
-class EZC3D_API ezc3d::ParametersNS::GroupNS::Group{
-public:
-    Group(const std::string &name = "", const std::string &description = "");
-
-    int read(ezc3d::c3d &file, int nbCharInName);
-    int addParameter(ezc3d::c3d &file, int nbCharInName);
-    void addParameter(const ezc3d::ParametersNS::GroupNS::Parameter& p);
-    void print() const;
-    void write(std::fstream &f, int groupIdx, std::streampos &dataStartPosition) const;
-
-    // Getter for the group
-    void lock();
-    void unlock();
-    bool isLocked() const;
-    const std::string& name() const;
-    const std::string& description() const;
-    const std::vector<ezc3d::ParametersNS::GroupNS::Parameter>& parameters() const;
-    const ezc3d::ParametersNS::GroupNS::Parameter& parameter(int idx) const;
-    std::vector<ezc3d::ParametersNS::GroupNS::Parameter>& parameters_nonConst();
-    int parameterIdx(std::string parameterName) const;
-    const ezc3d::ParametersNS::GroupNS::Parameter& parameter(std::string parameterName) const;
-
-protected:
-    bool _isLocked; // If the group should not be modified
-
-    std::string _name;
-    std::string _description;
-
-    std::vector<ezc3d::ParametersNS::GroupNS::Parameter> _parameters; // Holder for the parameters of the group
 };
 
 #endif
