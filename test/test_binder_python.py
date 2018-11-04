336f3f2aece2eb757303da965ef226e26d228a72
Pariterre <pariterre@hotmail.com>
2018-11-04 09:16:46 -0500

    added a non-wrking test to see the reaction of travis at a failure on the python test

---
+++ test/test_binder_python.py
@@ -0,0 +1,16 @@
+"""
+Test for file IO
+"""
+from pathlib import Path
+
+import numpy as np
+import pytest
+
+import ezc3d
+
+def test_create():
+    c3d = ezc3d.c3d()
+    assert 0 == 1
+    # test shape
+    # np.testing.assert_equal(0, expected_shape)
+
