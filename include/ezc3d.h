51bcd50674af0e6081760f6cdb3be3f586698a82
Pariterre <pariterre@hotmail.com>
2019-08-09 09:26:16 +0200

    Updated the Doc for the next release

--- include/ezc3d.h
+++ include/ezc3d.h
@@ -18,7 +18,7 @@
 /// this library is to eazily create, read and modify c3d (<a href="http://c3d.org">http://c3d.org</a>)
 /// files, largely used in biomechanics.
 ///
-/// This documentation was automatically generated for the version 0.4.0 on the 29th of May, 2019.
+/// This documentation was automatically generated for the "Nostalgia" Release 1.1.0 on the 9th of August, 2019.
 ///
 /// \section install_sec Installation
 ///
