2822ab6865a259bcbf751353603b6ca30e14d84c
Kilidsch <deniz-mo@hotmail.de>
2021-08-02 22:03:07 +0200

    Add regex for version in setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,21 @@
 from skbuild import setup
+import re
+import os
+dir_path = os.path.dirname(os.path.realpath(__file__))
+
+with open(f"{dir_path}/CMakeLists.txt") as file:
+    for line in file:
+        match = re.search("project\(ezc3d VERSION ([0-9].[0-9].[0-9])\)", line)
+        if match is not None:
+            version = match[1]
+            break
+    else:
+        raise RuntimeError("Version not found")
 
 setup(
     # NOTE: Could still add stuff like homepage or author mail, but since this isn't used to redistribute, not important
     name="ezc3d",
-    version="1.4.4", # NOTE: Can this be automatically extracted from CMakeLists.txt without manually regex'ing it out?
+    version=version,
     description=" Easy to use C3D reader/writer for C++, Python and Matlab",
     author="Michaud, Benjamin and Begon, Mickaël",
     license="MIT",
