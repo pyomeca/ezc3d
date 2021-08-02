bc1f78844c26bf965a1c40229efcd1b13abdf456
Kilidsch <deniz-mo@hotmail.de>
2021-08-02 16:55:21 +0200

    Restore old behaviour when not using skbuild

--- setup.py
+++ setup.py
@@ -2,13 +2,20 @@ from skbuild import setup
 
 # TODO Move swig stuff to command line
 setup(
+    # NOTE: Could still add stuff like homepage or author mail, but since this isn't used to redistribute, not important
     name="ezc3d",
-    version="1.0.0",
-    description="TODO",
-    author="TODO",
+    version="1.4.4", # NOTE: Can this be automatically extracted from CMakeLists.txt without manually regex'ing it out?
+    description=" Easy to use C3D reader/writer for C++, Python and Matlab",
+    author="Michaud, Benjamin and Begon, Mickaël",
     license="MIT",
     packages=['ezc3d'],
-    cmake_args=['-DBUILD_EXAMPLE:BOOL=OFF', '-DBINDER_PYTHON3:BOOL=ON', R"-DSWIG_DIR=D:\KleineProjekte\ezc3d\swigwin-4.0.2\Lib", R"-DSWIG_EXECUTABLE=D:\KleineProjekte\ezc3d\swigwin-4.0.2\swig.exe", '-DCMAKE_INSTALL_BINDIR="ezc3d"'],
-    #package_dir={"ezc3d": "binding/python3"},
-    #cmake_install_dir="out",
+    cmake_args=[
+        '-DBUILD_EXAMPLE:BOOL=OFF',
+        '-DBINDER_PYTHON3:BOOL=ON',
+        R"-DSWIG_DIR=D:\KleineProjekte\ezc3d\swigwin-4.0.2\Lib",
+        R"-DSWIG_EXECUTABLE=D:\KleineProjekte\ezc3d\swigwin-4.0.2\swig.exe",
+        '-DCMAKE_INSTALL_BINDIR="ezc3d"',
+        '-Dezc3d_BIN_FOLDER=ezc3d',
+        '-Dezc3d_LIB_FOLDER=ezc3d'
+    ],
 )
